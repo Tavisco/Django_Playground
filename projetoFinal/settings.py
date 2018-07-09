1b000d8401452045811b6fdc67fc7073c091ea9d
Tavisco <otavio.marpin@gmail.com>
2018-07-08 21:01:05 -0300

    Fix: Static files again

--- projetoFinal/settings.py
+++ projetoFinal/settings.py
@@ -80,22 +80,6 @@ WSGI_APPLICATION = 'projetoFinal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'gestao_clientes_udemy',
-#         'USER': 'postgres',
-#         'PASSWORD': 'otavio10',
-#         'HOST': '192.168.0.145',
-#         'PORT': '5432',
-#     }
-# }
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
@@ -138,6 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
